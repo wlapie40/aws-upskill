a219b12a113140b4efa298316d10ca01063af7cf
Szymon Figiel (PGS Software) <sfigiel@pgs-soft.com>
2019-09-19 10:09:48 +0200

    hotfix 1.0

--- deploy.py
+++ deploy.py
@@ -4,28 +4,27 @@ import time
 
 flask_env = os.environ.get('FLASK_ENV', False)
 subprocess.run('pip --version')
-subprocess.run('pipenv --version')
+try:
+    subprocess.run('pipenv --version')
+except:
+    subprocess.run('pip install pipenv')
 
-subprocess.run('pipenv shell')
 if flask_env:
-    subprocess.run('pipenv shell')
     subprocess.run('pipenv install -r requirements.txt')
-    # subprocess.run('python deploy.py')
-
+    print('======= generating creds for db =======')
     subprocess.run('python gen_creds_for_db.py')
     subprocess.run('docker-compose down')
 
     if flask_env == 'docker':
-        subprocess.run('docker-compose -f docker-compose-docker.yml up -d --force-recreate')
+        subprocess.run('docker-compose -f docker-compose-docker.yml up -d --force-recreate --remove-orphans')
     elif flask_env == 'dev':
         subprocess.run('docker-compose up -d --force-recreate --remove-orphans')
-        print('sleeping 3 sec')
-        time.sleep(3)
-        subprocess.run('python app.py')
     else:
         subprocess.run('docker-compose up -d')
-        print('sleeping 3 sec')
-        time.sleep(3)
-        subprocess.run('python app.py')
+
+    time.sleep(3)
+    print('sleeping...')
+    print('======= running flask =======')
+    subprocess.run('python app.py')
 else:
     raise KeyError('FLASK_ENV has not been set up :( ')
